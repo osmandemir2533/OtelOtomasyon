94f76d2bf0224d518bd01ba8e613e4552eb7dd41
Osman Demir <osman25dem2@gmail.com>
2024-12-17 15:52:09 +0300

    Proje dosyası ekle.

---
+++ OtelYeniProje/Formlar/Araclar/FrmYoutube.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OtelYeniProje.Formlar.Araclar
+{
+    public partial class FrmYoutube : Form
+    {
+        public FrmYoutube()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmYoutube_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://www.youtube.com");
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
